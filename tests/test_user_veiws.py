0f54e0d5dd7f026c2693440f654ab7099719fbb9
jennizalwango <jennyzalwango12@gmail.com>
2019-02-15 19:04:59 +0300

    modify code

--- tests/test_user_veiws.py
+++ tests/test_user_veiws.py
@@ -10,6 +10,7 @@ class TestUserVeiwsRoutes(BaseTestCase):
       self.assertEqual(response.status_code, 201)
       self.assertEqual(data["message"], "User created successfully")
 
+
   def test_user_register_with_wrong_content_type(self):
     with self.client:
       response = self.register_user_with_wrong_content_type("mercy", "mercy1", "mercy@gmail.com", 256739876542, False)
