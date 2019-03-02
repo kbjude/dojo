174d908824a8827125b4a50dd3b8b90965b9ff61
Mugaya Joel Patrick <43677190+PatrickMugayaJoel@users.noreply.github.com>
2019-03-02 08:28:29 +0300

    Update user_routes.py

--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -1,10 +1,6 @@
-from app.views.user_veiws import SignupUser
-from app.views.user_veiws import LoginUser
+from app.views.user_veiws import SignupUser, LoginUser
+from app import app
 
-class UserUrl:
-  @staticmethod
-  def get_user_routes(app):
-    register_user_view = SignupUser.as_view("create_user") 
-    log_user_view = LoginUser.as_view("login_a_user")
-    app.add_url_rule("/auth/signup", view_func= register_user_view, methods=["POST"])
-    app.add_url_rule("/auth/login", view_func= log_user_view, methods=["POST"])
+
+app.add_url_rule("/api/v1/auth/signup", view_func= SignupUser.as_view("signup"), methods=["POST"])
+app.add_url_rule("/api/v1/auth/login", view_func= LoginUser.as_view("login"), methods=["POST"])
