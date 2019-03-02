698283db162d3f7c3fd7144431f509667b34d097
Mugaya Joel Patrick <43677190+PatrickMugayaJoel@users.noreply.github.com>
2019-03-02 08:27:47 +0300

    Update incident_routes.py
    I got rid of classes and methods. (requires peer review)

--- app/routes/incident_routes.py
+++ app/routes/incident_routes.py
@@ -1,26 +1,11 @@
-from app.views.incident_veiws import CreateIncident
-from app.views.incident_veiws import Updates
-from app.views.incident_veiws import Status
-
-class IncidentUrl:
-  @staticmethod
-  def get_incident_routes(app):
-    create_incident_veiw = CreateIncident.as_view("create_an_incident")
-    app.add_url_rule("/incident", view_func=create_incident_veiw, methods=["POST"])
-    app.add_url_rule("/incident/<int:incident_id>", view_func=create_incident_veiw, methods=["GET"])
-    app.add_url_rule("/incident", view_func=create_incident_veiw, defaults={"incident_id": None}, methods=["GET"])
-    app.add_url_rule("/incident/<int:incident_id>/location", view_func=create_incident_veiw, methods=["PUT"])
+from app.views.incident_veiws import CreateIncident, Updates, Status
+from app import app
 
 
-class UpdateUrl:
-  @staticmethod
-  def update_the_comment_routes(app):
-    Update_comment_veiw = Updates.as_view("update_a_comment")
-    app.add_url_rule("/incident/<int:incident_id>/comment", view_func=Update_comment_veiw, methods=["PUT"])
-
-class UpdateStatus:
-  @staticmethod
-  def update_status(app):
-    Update_status_veiw = Status.as_view("update_a_status")
-    app.add_url_rule("/incident/<int:incident_id>/status", view_func=Update_status_veiw, methods=["PUT"])
-    app.add_url_rule("/incident/<int:incident_id>", view_func=Update_status_veiw, methods=["DELETE"])
+app.add_url_rule("/api/v1/incident", view_func=CreateIncident.as_view("create"), methods=["POST"])
+app.add_url_rule("/api/v1/incident/<int:incident_id>", view_func=CreateIncident.as_view("get_incident"), methods=["GET"])
+app.add_url_rule("/api/v1/incident", view_func=CreateIncident.as_view("get_incidents"), defaults={"incident_id": None}, methods=["GET"])
+app.add_url_rule("/api/v1/incident/<int:incident_id>/location", view_func=CreateIncident.as_view("update_location"), methods=["PUT"])
+app.add_url_rule("/api/v1/incident/<int:incident_id>/comment", view_func=Updates.as_view("update_comment"), methods=["PUT"])
+app.add_url_rule("/api/v1/incident/<int:incident_id>/status", view_func=Status.as_view("update_status"), methods=["PUT"])
+app.add_url_rule("/api/v1/incident/<int:incident_id>", view_func=Status.as_view("delete_incident"), methods=["DELETE"])
