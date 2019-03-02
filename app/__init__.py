54971f61aa74e964e53e8b434eed7035dd0000c0
Mugaya Joel Patrick <43677190+PatrickMugayaJoel@users.noreply.github.com>
2019-03-02 08:16:30 +0300

    Update __init__.py
    - I moved database functionality out of this file to a database module.
    - I added configurations from config.py into flask's app.config
    - I tweaked the logs appearance
    - I added an index route
    - Imported user_routes and incident_routes as opposed to running them as functions. (Requires peer review)

--- app/__init__.py
+++ app/__init__.py
@@ -1,35 +1,34 @@
 import os
-import psycopg2
 from flask import Flask
-from passlib.hash import sha256_crypt
-from app.config import configuration
+from logging.config import dictConfig
+
 
 app = Flask(__name__)
-config = configuration.get(os.environ.get("APP_ENV", "development"))
-app.config.from_object(config)
-
-DatabaseConnection = psycopg2.connect(
-  database = config.DATABASE_NAME, 
-  password = config.DATABASE_PASSWORD, 
-  user = config.DATABASE_USER, 
-  host = config.DATABASE_HOST, 
-  port = 5432
-  )
-
-# create_db_tables()
-# if os.getenv() === ''
+app.config.from_object(os.environ['APP_SETTINGS'])
 
 
-# DatabaseConnection = psycopg2.connect(database ="testdb")
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+})
 
-from app.routes.user_routes import UserUrl
-UserUrl.get_user_routes(app)
 
-from app.routes.incident_routes import IncidentUrl
-IncidentUrl.get_incident_routes(app)
+#index route
+@app.route('/')
+def index():
+    """ index route """
+    return "Welcome. This is a remix by the dojos.", 200
 
-from app.routes.incident_routes import UpdateUrl
-UpdateUrl.update_the_comment_routes(app)
 
-from app.routes.incident_routes import UpdateStatus
-UpdateStatus.update_status(app)
+from app.routes import user_routes, incident_routes
